445921a4e27ab9e15e0985525b8b8632c20a35fc
Liweifei <1195669615@qq.com>
2019-07-19 15:03:23 +0800

    优化多人同时登录，在差不多时间发起抽奖时都可选择bug

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -8,13 +8,13 @@ var collectionName = "restaurant";
 /* save restaurant. */
 router.post('/save', function (req, res, next) {
   if (req.body.name && req.body.location) {
-    dbTool.findByStr(collectionName,{"user": req.get("Authorization")}, function (type, arr) {
+    dbTool.findByStr(collectionName, { "user": req.get("Authorization") }, function (type, arr) {
       if (type) {
         if (arr.length >= 8) {
           res.json(jsonTool.justCodeInt(false, "只能加入8条数据哦！"))
           return;
         }
-        dbTool.findOne(collectionName, { "name": req.body.name,"user": req.get("Authorization") }, function (resons) {
+        dbTool.findOne(collectionName, { "name": req.body.name, "user": req.get("Authorization") }, function (resons) {
           if (!!resons) {
             res.json(jsonTool.justCodeInt(false, "饭店已存在哦！"))
           } else {
@@ -39,7 +39,7 @@ router.post('/save', function (req, res, next) {
 /* remove restaurant. */
 router.post('/delete', function (req, res, next) {
   if (req.body._id) {
-    console.log(new ObjectId(req.body._id),req.body._id)
+    console.log(new ObjectId(req.body._id), req.body._id)
     var whereStr = { '_id': new ObjectId(req.body._id) };
     dbTool.deleteOne(collectionName, whereStr, function (result) {
       var msg = result ? '删除成功！' : '删除失败！';
@@ -75,24 +75,34 @@ router.post('/update', function (req, res, next) {
 /* draw this restuarant. */
 router.post('/draw', function (req, res, next) {
   if (req.body._id) {
-    var whereStr = { '_id': new ObjectId(req.body._id) };
-    var updateStr = {
-      $set:
-      {
-        "isSelected": 1,
-        "isToday":true
-      }
-    }
-    dbTool.updateOne(collectionName, whereStr, updateStr, function (result) {
-      var updateStr2 = { $set: { "drawType": true } }
-      dbTool.updateMany("token", {}, updateStr2, function (results) {
-        var msg = result ? '抽选成功！' : '抽选失败！';
-        res.json(jsonTool.justCodeInt(result, msg))
-      })
+    dbTool.findOne("token", { token: req.get("Authorization") }, function (results) {
+      if (typeof (results) == "object") {
+        if (!results.drawType) {
+          var whereStr = { '_id': new ObjectId(req.body._id) };
+          var updateStr = {
+            $set:
+            {
+              "isSelected": 1,
+              "isToday": true
+            }
+          }
+          dbTool.updateOne(collectionName, whereStr, updateStr, function (result) {
+            var updateStr2 = { $set: { "drawType": true } }
+            dbTool.updateOne("token", { token: req.get("Authorization") }, updateStr2, function (results) {
+              var msg = result ? '抽选成功！' : '抽选失败！';
+              res.json(jsonTool.justCodeInt(result, msg))
+            })
 
+          })
+        } else {
+          res.json(jsonTool.toObj({ type: "havedrawed" }))
+        }
+      } else {
+        res.json(jsonTool.justCodeInt(false, "未找到该条数据"))
+      }
     })
   } else {
-    res.json(jsonTool.justCodeInt(false, "未找到该条数据"))
+    res.json(jsonTool.justCodeInt(false, "获取参数失败"))
   }
 });
 
